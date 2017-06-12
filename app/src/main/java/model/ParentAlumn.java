d7d29a69de4e113c66921d1112cce38dfcbd5c1f
MrVictor42 <guin-hu@hotmail.com>
2017-06-12 13:34:30 -0300

    Update gitignore

---
+++ app/src/main/java/model/ParentAlumn.java
@@ -0,0 +1,126 @@
+package model;
+
+public class ParentAlumn {
+
+    private String nameAlumn;
+    private String registryAlumn;
+    private Integer alumnId;
+    private Integer idParent;
+    private String nameParent;
+    private String phoneParent;
+    private Integer idStrike;
+    private String description_strike;
+    private String date_strike;
+    private String title;
+    private String description;
+    private Integer quantity_days;
+    private Integer idSuspension;
+
+    public ParentAlumn () {
+
+    }
+
+    public String getNameAlumn() {
+        return nameAlumn;
+    }
+
+    public void setNameAlumn(String nameAlumn) {
+        this.nameAlumn = nameAlumn;
+    }
+
+    public String getRegistryAlumn() {
+        return registryAlumn;
+    }
+
+    public void setRegistryAlumn(String registryAlumn) {
+        this.registryAlumn = registryAlumn;
+    }
+
+    public Integer getAlumnId() {
+        return alumnId;
+    }
+
+    public void setAlumnId(Integer alumnId) {
+        this.alumnId = alumnId;
+    }
+
+    public Integer getIdParent() {
+        return idParent;
+    }
+
+    public void setIdParent(Integer idParent) {
+        this.idParent = idParent;
+    }
+
+    public String getNameParent() {
+        return nameParent;
+    }
+
+    public void setNameParent(String nameParent) {
+        this.nameParent = nameParent;
+    }
+
+    public String getPhoneParent() {
+        return phoneParent;
+    }
+
+    public void setPhoneParent(String phoneParent) {
+        this.phoneParent = phoneParent;
+    }
+
+    public Integer getIdStrike() {
+        return idStrike;
+    }
+
+    public void setIdStrike(Integer idStrike) {
+        this.idStrike = idStrike;
+    }
+
+    public String getDescription_strike() {
+        return description_strike;
+    }
+
+    public void setDescription_strike(String description_strike) {
+        this.description_strike = description_strike;
+    }
+
+    public String getDate_strike() {
+        return date_strike;
+    }
+
+    public void setDate_strike(String date_strike) {
+        this.date_strike = date_strike;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getQuantity_days() {
+        return quantity_days;
+    }
+
+    public void setQuantity_days(Integer quantity_days) {
+        this.quantity_days = quantity_days;
+    }
+
+    public Integer getIdSuspension() {
+        return idSuspension;
+    }
+
+    public void setIdSuspension(Integer idSuspension) {
+        this.idSuspension = idSuspension;
+    }
+}
